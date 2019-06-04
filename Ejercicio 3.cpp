d7179379e273a3e705eeb4a3213c7f916c9645e5
Augusto Morante <48858576+AugustoMorante@users.noreply.github.com>
2019-06-04 09:21:58 -0500

    Add files via upload

---
+++ Ejercicio 3.cpp
@@ -0,0 +1,55 @@
+// ConsoleApplication4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void menu() {
+
+	cout << "OPCIONES" << endl;
+	cout << "Aniadir una ficha nueva [1]" << endl;
+	cout << "Ver la Informacion de todas las fichas [2]" << endl;
+	cout << "Buscar Ficha de cierto Tipo [3]" << endl;
+	cout << "Finalizar [4]" << endl;
+
+	return;
+}
+
+typedef struct
+{
+	char categoria;
+	int edad;
+} datosPersonal;
+
+int main()
+{
+	datosPersonal *Personal;
+	Personal = new datosPersonal[10];
+	
+	for (int i = 0; i < 5; i++)
+	{
+		cout <<"Trabajador "<< i + 1 << " Ingrese categoria " << ": "; cin >> Personal[i].categoria;
+		cout << "Ingrese edad "  << ": "; cin >> Personal[i].edad;
+		
+	}
+
+	char tipo;
+
+	cout << "Ingrese categoria buscar " << endl;
+	cin >> tipo;
+
+	for (int i=0;i < 5 ;i++) {
+
+		if (Personal[i].categoria == tipo)
+		{
+			cout<<"Empleado "<<i+1 << " Categoria : "<< ": " << Personal[i].categoria << endl;
+			cout<< "Edad : " << ": " << Personal[i].edad << endl;
+			
+		}
+	}
+	_getch();
+}
+
